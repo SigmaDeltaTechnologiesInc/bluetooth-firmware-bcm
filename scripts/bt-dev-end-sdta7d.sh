f8a5bbb8699b45c2b8a21b392e30f68ac5c6ac23
Hackseung Lee <lhs@dignsys.com>
2019-03-05 10:45:32 +0900

    script:sdta7d
    Signed-off-by: Hackseung Lee <hackseung@dignsys.com>

--- scripts/bt-dev-end-sdta7d.sh
+++ scripts/bt-dev-end-sdta7d.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-# Device down
-/usr/bin/hciconfig hci0 down
+HCI_CONFIG=/usr/bin/hciconfig
+HCI_DEV=hci0
+
+$HCI_CONFIG | /usr/bin/grep $HCI_DEV &> /dev/null
+if [ $? -eq 0 ]; then
+	$HCI_CONFIG $HCI_DEV down
+fi
--- scripts/bt-dev-start-sdta7d.sh
+++ scripts/bt-dev-start-sdta7d.sh
@@ -1,50 +1,26 @@
 #!/bin/sh
-PATH=/bin:/usr/bin:/sbin:/usr/sbin
 
-# Script for registering Broadcom UART BT device
 BT_UART_DEVICE=/dev/ttymxc6
 BT_CHIP_TYPE=bcm43xx
-BT_MAC_FILE=/opt/etc/.bd_addr
-
-BT_PLATFORM_DEFAULT_HCI_NAME="TIZEN-Mobile"
-UART_SPEED=3000000
-
+BT_DEFAULT_HCI_NAME="TIZEN-SDTA7D"
 HCI_CONFIG=/usr/bin/hciconfig
 HCI_ATTACH=/usr/bin/hciattach
+HCI_DEV=hci0
 
-GEN_BDADDR(){
-        echo "BT Mac addr generates randomly."
-        MAC_PRE=$(echo "00:02:")
-        MAC_POST=$(/usr/bin/openssl rand -hex 4 | sed 's/\(..\)/\1:/g; s/.$//')
-        echo "Random : $MAC_PRE$MAC_POST"
-        BT_MAC=$MAC_PRE$MAC_POST
-}
-
-if [ ! -e "$BT_MAC_FILE" ]
-then
-	# Set BT address
-	GEN_BDADDR
-	echo $BT_MAC > ${BT_MAC_FILE}
+$HCI_CONFIG | /usr/bin/grep $HCI_DEV &> /dev/null
+if [ $? -eq 0 ]; then
+	$HCI_CONFIG $HCI_DEV up;
 else
-	BT_MAC=$(cat ${BT_MAC_FILE})
-fi
+	/usr/bin/lsmod | /usr/bin/grep "hci_uart" &> /dev/null
+	if [ $? -ne 0 ]; then
+		/usr/sbin/modprobe hci_uart
+	fi	
 
-echo $BT_MAC
+	if ($HCI_ATTACH $BT_UART_DEVICE $BT_CHIP_TYPE); then
+		$HCI_CONFIG $HCI_DEV up
+		$HCI_CONFIG $HCI_DEV name $BT_DEFAULT_HCI_NAME
+		$HCI_CONFIG $HCI_DEV sspmode 1
 
-echo "Check for Bluetooth device status"
-if ($HCI_CONFIG | grep hci); then
-	echo "Bluetooth device is UP"
-	$HCI_CONFIG hci0 up
-else
-	echo "Bluetooth device is DOWN"
-	echo "Registering Bluetooth device"
-
-	# Attaching Broadcom device
-	if ($HCI_ATTACH $BT_UART_DEVICE $BT_CHIP_TYPE $UART_SPEED noflow nosleep $BT_MAC); then
-		sleep 0.1
-		$HCI_CONFIG hci0 up
-		$HCI_CONFIG hci0 name $BT_PLATFORM_DEFAULT_HCI_NAME
-		$HCI_CONFIG hci0 sspmode 1
 		echo "HCIATTACH success"
 	else
 		echo "HCIATTACH failed"
